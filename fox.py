733818920b7be2dd06fbe9d1817ccaa1b6c876cc
Admin <admin@Admins-MacBook-Pro-10.local>
2024-02-26 14:19:55 +0530

    feat-2 commit

---
+++ fox.py
@@ -0,0 +1,13 @@
+class Fox:
+     def eat(self):
+        print("I can Eat")
+
+     def sleep(self):
+         print("I can Sleep")
+
+     def sound(self):
+         print("I can sound")
+
+    def run(self):
+        print("I can Run")
+
