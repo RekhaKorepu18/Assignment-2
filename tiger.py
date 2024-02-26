188ca97da2ed8f537468c52e2ebca019d5c0f01d
Admin <admin@Admins-MacBook-Pro-10.local>
2024-02-26 12:59:22 +0530

    Tiger is Created

---
+++ tiger.py
@@ -0,0 +1,12 @@
+class tiger:
+     def eat(self):
+        print("I can Eat")
+
+     def sleep(self):
+         print("I can Sleep")
+
+     def roar(self):
+         print("I can roar")
+
+    def run(self):
+        print("I can Run")
